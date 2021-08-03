fc429324329efe1228e531fc22cab61515a34731
PL-8\ladmin <ali.haider@pentaloop.com>
2021-08-03 12:47:32 +0500

    Added check for null or undefined channel id

--- index.js
+++ index.js
@@ -19,18 +19,30 @@ const io = socketio(server,{
 
 // Run when client connects
 io.on('connection', socket => {
-    
+    //console.log('Printing io Sockets : ',io.sockets.adapter.rooms)
+
     // Creating Channel : channelId Format companyId_channel_page (8_channel_visit)
     socket.on('joinChannel',({channelId})=>{
+        if (channelId){
         //check if we successfully joined with channel
         socket.join(channelId)
         socket.emit('connection',{message : `You have Successfully Connected To Socket Server to channel ${channelId}`})
+        } else {
+              // emit handler that you channel id is not a valid id
+            socket.emit('customError',{message : 'Channel Id is not defined'})  
+        }
+      
     })
 
     // Ping From ignition_web_server for an update
     socket.on('update',({channelId})=>{
         // Based on channelId Broadcast to all the clients of a specific channel except sender
-        socket.to(channelId).emit('call',{message : `You Need to Make Api call For ${channelId}`})
+        if (channelId){
+            socket.to(channelId).emit('call',{message : `You Need to Make Api call For ${channelId}`})
+        } else {
+            // emit handler that you channel id is not a valid id
+            socket.emit('customError',{message : 'Channel Id is not defined'})  
+        }
     })
 
     // Runs when client disconnects
